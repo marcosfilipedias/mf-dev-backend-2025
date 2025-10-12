b1e526fcce1f64e06daf0f64ee96951e7c2de7f2
Marcos Filipe <marcosfilipe.dm@gmail.com>
2025-10-12 12:15:05 -0300

    Configuracao banco de dados e tabela veiculo

---
+++ mf-dev-backend-2025/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2025.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Placa é obrigatório.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O campo Ano de Fabricação é obrigatório.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "O campo Ano do Modelo é obrigatório.")]
+        public int AnoModelo { get; set; }
+    }
+}
